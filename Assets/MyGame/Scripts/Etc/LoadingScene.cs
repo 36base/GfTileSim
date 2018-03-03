03d4a65635f73a314cb7652986903774879ae670
COSMOS-PC\COSMOS <cosmicmosmic@gmail.com>
2018-03-03 23:55:46 +0900

    로드씬 제작중

---
+++ Assets/MyGame/Scripts/Etc/LoadingScene.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    public Slider slider;
+
+    bool load = false;
+    AsyncOperation async_operation;
+
+    public Text loadText;
+
+    private void Update()
+    {
+        loadText.color 
+            = new Color(loadText.color.r, loadText.color.g, loadText.color.b, Mathf.PingPong(Time.time, 1));
+
+        if (!load)
+        {
+            load = true;
+            StartCoroutine(StartLoad(1));
+        }
+    }
+
+    public IEnumerator StartLoad(int scene)
+    {
+        async_operation = SceneManager.LoadSceneAsync(scene);
+
+        async_operation.allowSceneActivation = false;
+
+        while (!async_operation.isDone)
+        {
+            //Debug.Log(async_operation.progress);
+            slider.value = async_operation.progress;
+            yield return null;
+        }
+        slider.value = 1f;
+        Debug.Log("hi");
+        async_operation.allowSceneActivation = true;
+    }
+}
