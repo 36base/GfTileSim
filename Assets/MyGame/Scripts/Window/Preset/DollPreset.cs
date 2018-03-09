58de8c2e411ad8bbc93ea1078ce2fff05c2e4b76
COSMOS-PC\COSMOS <cosmicmosmic@gmail.com>
2018-03-09 21:41:24 +0900

    제대프리셋 틀

---
+++ Assets/MyGame/Scripts/Window/Preset/DollPreset.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DollPreset : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject deletePanel;
+    public bool pressed;
+
+    public float longTouchTime = 1.5f;
+    private float currTime;
+
+    private void Update()
+    {
+        if (pressed)
+        {
+            currTime += Time.deltaTime;
+            if (currTime > longTouchTime)
+            {
+                deletePanel.SetActive(true);
+            }
+        }
+        else
+        {
+            currTime = 0f;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+
+    public void Preset2Grid()
+    {
+
+    }
+
+    public void Grid2Preset()
+    {
+
+    }
+
+    public void DeletePreset()
+    {
+        deletePanel.SetActive(false);
+    }
+    public void CancelDelete()
+    {
+        deletePanel.SetActive(false);
+    }
+}
