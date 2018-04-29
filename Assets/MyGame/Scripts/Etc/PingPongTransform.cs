a577b631a014d7877a6d6d3faf8145bca9810a86
COSMOS-PC\COSMOS <cosmicmosmic@gmail.com>
2018-04-29 19:16:10 +0900

    페일 클리어 추가 광고배너 테스트

---
+++ Assets/MyGame/Scripts/Etc/PingPongTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongTransform : MonoBehaviour
+{
+    public Transform tr;
+
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+    private void Start()
+    {
+        tr.localPosition = startPos;
+    }
+
+    void Update()
+    {
+        tr.localPosition = Vector3.Lerp(startPos, endPos, Mathf.PingPong(Time.time * 0.1f, 1f));
+    }
+}
